320ebeb7154b00be9e3cd53231ba0ae36920b6ab
Hamza Muhumed <hamzamuhumed2107@gmail.com>
2023-06-26 11:34:06 -0500

    Set up the router GET to get data from api for league standings

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const teamRouter = require('./routes/team.router');
+const leagueRouter = require('./routes/league.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,8 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/team', teamRouter);
+app.use('/api/standings', leagueRouter);
+
 
 // Serve static files
 app.use(express.static('build'));
